190d347bc209931301de57e0ac3317f359f9cc16
Rina Tamrakar <rina@Rinas-MacBook-Pro.local>
2020-03-30 23:07:05 -0500

    Login page WIP

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
         <p>
             <router-link to="/">Home</router-link>
             <router-link to="/login">Login</router-link>
+            <router-link to="/register">Register</router-link>
         </p>
           <router-view/>
       </div>
--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './components/Home.vue'
 import Login from './components/Login.vue'
+import Register from './components/Register.vue'
 
 const routes = [
     {path: '/', component: Home},
     {path: '/login', component: Login},
+    {path: '/register', component: Register},
 ]
 
 Vue.use(VueRouter)
