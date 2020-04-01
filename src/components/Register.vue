164c2f836bf5999a008fe0a046e21f8604a17455
Rina Tamrakar <rina@Rinas-MacBook-Pro.local>
2020-03-31 20:28:38 -0500

    Register Pages

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,32 +1,46 @@
 <template>
-  <div class="hello">
-    <h1>Register</h1>
-   </div>
+ <div class="body">
+  <div class="row">
+    <div class = "six columns" > 
+     	<router-link to="/user-register" class="button button-primary">Register as a user</router-link>
+  		<div class = "six columns">
+ 				<router-link to="/doctor-register" class="button button-primary">Register as a doctor</router-link>
+			</div>
+<!-- Always wrap checkbox and radio inputs in a label and use a <span class="label-body"> inside of it -->
+		</div>
+    	<div class="two columns"></div>
+   
+
+  </div>
+ </div>
 </template>
 
 <script>
-export default {
-  name: 'Login',
-  props: {
-    msg: String
-  }
-}
+	export default {
+	  name: 'Register',
+	  props: {
+	    msg: String
+	  }
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+	h3 {
+	  margin: 40px 0 0;
+	}
+	ul {
+	  list-style-type: none;
+	  padding: 0;
+	}
+	li {
+	  display: inline-block;
+	  margin: 0 10px;
+	}
+	a {
+	  color: #42b983;
+	}
+	.body{
+	  background-color: #cbd2d7;
+	}
 </style>
--- src/router.js
+++ src/router.js
@@ -3,11 +3,15 @@ import VueRouter from 'vue-router'
 import Home from './components/Home.vue'
 import Login from './components/Login.vue'
 import Register from './components/Register.vue'
+import UserRegister from './components/UserRegister.vue'
+import DoctorRegister from './components/DoctorRegister.vue'
 
 const routes = [
     {path: '/', component: Home},
     {path: '/login', component: Login},
     {path: '/register', component: Register},
+    {path: '/user-register', component: UserRegister},
+    {path: '/doctor-register', component: DoctorRegister},
 ]
 
 Vue.use(VueRouter)
